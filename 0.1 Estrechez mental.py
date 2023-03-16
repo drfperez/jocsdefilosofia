f6f97f9d0a2d60abd229b5b5f0f5a376cdc9de73
Francisco Pérez <drfperez@gmail.com>
2023-03-16 09:37:15 +0100

    Update 0.1 Estrechez mental.py

--- 0.1 Estrechez mental.py
+++ 0.1 Estrechez mental.py
@@ -4,7 +4,7 @@ import time
 def pantalla_inicial():
     pygame.init()
     pantalla = pygame.display.set_mode((800, 600))
-    fuente = pygame.font.SysFont(None, 50)
+    fuente = pygame.font.SysFont(None, 36)
     mensaje1 = fuente.render("Resetea tus ideas o morirás", True, (255, 255, 255))
     mensaje2 = fuente.render("aplastado por tu estrechez mental.", True, (255, 255, 255))
     mensaje3 = fuente.render("Solo una tecla te permitirá empezar desde cero...", True, (255, 255, 255))
@@ -23,14 +23,14 @@ def pantalla_final(ganaste):
         mensaje = fuente.render("Pienso luego existo", True, (255, 255, 255))
     else:
         mensaje = fuente.render("Muerto por no pensar", True, (255, 255, 255))
-    pantalla.blit(mensaje, (300, 200))
+    pantalla.blit(mensaje, (200, 200))
     pygame.display.update()
     time.sleep(3)
     pygame.quit()
 
 pantalla_inicial()
 
-ancho = 1000 
+ancho = 1000
 ganaste = False
 while ancho > 0 and not ganaste:
     ancho -= 100
